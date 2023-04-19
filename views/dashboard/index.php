9d6eda5959e23348f7f33baa2c82d068ac3a8a83
mikev-cw <113628339+mikev-cw@users.noreply.github.com>
2023-04-19 10:34:26 +0200

    updated dashboard and menu

--- views/dashboard/index.php
+++ views/dashboard/index.php
@@ -17,7 +17,7 @@
 <div class="content">
         <div class="container-fluid">
             <div class="row">
-                <div class="col-lg-6">
+                <div class="col-lg-6 col-6">
 
                     <div class="small-box bg-info">
                         <div class="inner">
@@ -27,7 +27,7 @@
                         <div class="icon">
                             <i class="ion ion-bag"></i>
                         </div>
-                        <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
+                        <a href="<?=URL?>demo" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                     </div>
                 </div>
 
@@ -73,19 +73,18 @@
                     </div>
                 </div>
 
-
-
-
-                <div class="card card-success card-outline">
-                        <div class="card-header">
-                            <h3 class="card-title">Just a BOX</h3>
-                        </div>
-                        <div class="card-body">
-                            <p class="card-text">
-                                Hey! :)
-                            </p>
-                        </div>
-                    </div><!-- /.card -->
+                <div class="col-lg-6">
+                    <div class="card card-success card-outline">
+                            <div class="card-header">
+                                <h3 class="card-title">Just a BOX</h3>
+                            </div>
+                            <div class="card-body">
+                                <p class="card-text">
+                                    Hey! :)
+                                </p>
+                            </div>
+                        </div><!-- /.card -->
+                    </div>
                 </div>
             </div>
         </div>
